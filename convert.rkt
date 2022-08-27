b37ecbc38ef6a00928afe7dc85a27b5862d34c44
Brian Leung <leungbk@posteo.net>
2022-08-26 21:12:36 -0700

    Keep the hash table immutable

--- convert.rkt
+++ convert.rkt
@@ -8,39 +8,28 @@
   (with-output-to-file output-path
     (lambda ()
       (let* ((inp0 (with-input-from-file input-path read))
-             (inp1 (convert-immutable-to-mutable-hash-table inp0))
-             (inp2 (reshape-dependencies inp1))
-             (inp3 (string-keys-to-symbol-keys inp2))
-             (inp4 (symbol-values-to-string-values inp3)))
-        (write-json inp4)))
+             (inp1 (reshape-dependencies inp0))
+             (inp2 (string-keys-to-symbol-keys inp1))
+             (inp3 (symbol-values-to-string-values inp2)))
+        (write-json inp3)))
     #:exists 'replace))
 
-(define (convert-immutable-to-mutable-hash-table value)
-  (cond
-    [(hash? value) (hash-map/copy value
-                                  (lambda (k v)
-                                    (values k
-                                            (convert-immutable-to-mutable-hash-table v)))
-                                  #:kind 'mutable)]
-    [(list? value) (map convert-immutable-to-mutable-hash-table value)]
-    [else value]))
-
 (define (reshape-dependencies ht)
-  (hash-for-each ht
+  (hash-map/copy ht
                  (lambda (pkg-name pkg-hash-table)
-                   (hash-update! pkg-hash-table
-                                 'dependencies
-                                 (lambda (dependencies)
-                                   (make-hash (map (lambda (dep)
-                                                     (if (string? dep)
-                                                         `(,dep . "default")
-                                                         (let ((dep-name (car dep))
-                                                               (version (if (memq '#:version dep)
-                                                                            (cadr (memq '#:version dep))
-                                                                            "default")))
-                                                           `(,dep-name . ,version))))
-                                                   dependencies))))))
-  ht)
+                   (values pkg-name
+                           (hash-update pkg-hash-table
+                                        'dependencies
+                                        (lambda (dependencies)
+                                          (make-immutable-hash (map (lambda (dep)
+                                                                      (if (string? dep)
+                                                                          `(,dep . "default")
+                                                                          (let ((dep-name (car dep))
+                                                                                (version (if (memq '#:version dep)
+                                                                                             (cadr (memq '#:version dep))
+                                                                                             "default")))
+                                                                            `(,dep-name . ,version))))
+                                                                    dependencies))))))))
 
 (define (string-keys-to-symbol-keys expr)
   (cond
