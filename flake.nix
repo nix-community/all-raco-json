52e1d3fbf42e29640db27f6353ffff000e70b322
Brian Leung <leungbk@posteo.net>
2022-09-10 22:55:07 -0700

    Try to use gha path of pkgs-all in write-catalog

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
         pruner = pkgs.writeScriptBin "prune-catalog" ''
           #!/usr/bin/env bash
           set -eou pipefail
-          ${pkgs.racket}/bin/racket -e '(require (file "convert.rkt")) (write-catalog "pkgs-all")'
+          ${pkgs.racket}/bin/racket -e '(require (file "convert.rkt")) (write-catalog "$1")'
         '';
       in
         {
