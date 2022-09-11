82536551646d5e91402a88460858908e52eea787
Brian Leung <leungbk@posteo.net>
2022-09-10 23:04:45 -0700

    Try to make write-catalog.rkt script a flake input

--- flake.nix
+++ flake.nix
@@ -4,10 +4,13 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
 
+    write-catalog.url = "path:write-catalog.rkt";
+    write-catalog.flake = false;
+
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ...}:
+  outputs = { self, nixpkgs, flake-utils, write-catalog, ...}:
     flake-utils.lib.eachSystem [ flake-utils.lib.system.x86_64-linux ] (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -16,7 +19,7 @@
         pruner = pkgs.writeScriptBin "prune-catalog" ''
           #!/usr/bin/env bash
           set -eou pipefail
-          ${pkgs.racket}/bin/racket -e '(require (file "write-catalog.rkt")) (write-catalog "$1")'
+          ${pkgs.racket}/bin/racket -e '(require (file "${write-catalog}")) (write-catalog "$1")'
         '';
       in
         {
